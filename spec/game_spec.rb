43ea993bb4f7a8cd5c2a28def954f1e98ac3056c
VKuzmich <vjk1976@ukr.net>
2019-07-17 16:19:45 +0300

    adding game_spec.rb

---
+++ spec/game_spec.rb
@@ -0,0 +1,24 @@
+require_relative 'spec_helper'
+require_relative '../dependencies'
+
+RSpec.describe Game do
+  subject(:game) { described_class.new(name: 'Rspec', difficulty: 'Easy') }
+
+  describe '.check' do
+    it 'returns ++++' do
+      expect(game.check(game.secret)).to eq('++++')
+    end
+  end
+
+  describe '#use_hint' do
+    it 'decrements hints and calls a hint method if hints > 0' do
+      game.instance_variable_set(:@hints, 1)
+      expect { game.use_hint }.to change(game, :hints).by(-1)
+    end
+
+    it 'returns a message if no hints left' do
+      game.instance_variable_set(:@hints, 0)
+      expect(game.use_hint).to eq('You have no hints')
+    end
+  end
+end
