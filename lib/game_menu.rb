14d2a6f52b3f092be5519fd5c5b73e00ce1e37e0
VKuzmich <vjk1976@ukr.net>
2019-08-14 12:33:22 +0300

    changing path file seed.yaml

--- lib/game_menu.rb
+++ lib/game_menu.rb
@@ -63,7 +63,7 @@ class GameMenu
     end
 
     def stats
-      return puts I18n.t(:no_stats) unless File.exist?('seed.yaml')
+      return puts I18n.t(:no_stats) unless File.exist?('./spec/fixtures/seed.yaml')
 
       table = load.sort_by { |row| [row.hints_total, row.attempts_used] }
       table.each { |row| row.difficulty = Game::DIFFICULTY_LEVEL.key([row.attempts_total, row.hints_total]) }
