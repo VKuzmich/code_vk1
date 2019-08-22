a03b17a69b12db15ea1403dc2be614fb1a445967
Vladimir Kuzmich <vjk1976@ukr.net>
2019-08-22 15:56:13 +0300

    refactoring

--- lib/validation.rb
+++ lib/validation.rb
@@ -7,8 +7,4 @@ module Validation
   def valid_string_length?(name, min = MIN_LETTERS, max = MAX_LETTERS)
     name.is_a?(String) && name.length.between?(min, max)
   end
-
-  def match_pattern?(guess)
-    GameConsole::INPUT_DATA.match(guess, Game::SECRET_CODE_LENGTH)
-  end
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -25,16 +25,4 @@ RSpec.describe Validation do
       expect(game_class.valid_string_length?(123)).to eq(false)
     end
   end
-
-  describe '.match_pattern??' do
-    it 'returns true cause of correct data' do
-      expect(game_class.match_pattern?('1234')) == (true)
-    end
-
-    it 'returns false cause of incorrect data' do
-      %w[123 12345 0123 3210 1237].each do |incorrect_data|
-        expect(game_class.match_pattern?(incorrect_data)) == (false)
-      end
-    end
-  end
 end
